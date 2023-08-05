3e2d6cf131f86fdd9bd6e08c8bb31364f752a8d4
Lloyd Allen <lloydallen597@gmail.com>
2023-08-04 20:43:56 -0500

    Added Audio Assets

---
+++ audio.js
@@ -0,0 +1,23 @@
+const audio = {
+  backgroundMusic: new Howl({
+    src: "./audio/Background-Music.wav",
+  }),
+  //   bomb: new Howl({
+  //     src: "./audio/bomb.mp3",
+  //   }),
+  //   bonus: new Howl({
+  //     src: "./audio/bonus.mp3",
+  //   }),
+  shootEnemy: new Howl({
+    src: "./audio/shootEnemy.mp3",
+  }),
+  Damage_taken: new Howl({
+    src: "./audio/Damage_taken.mp3",
+  }),
+  Death: new Howl({
+    src: "./audio/Death.mp3",
+  }),
+  playerShoot: new Howl({
+    src: "./audio/playerShoot.wav",
+  }),
+};
